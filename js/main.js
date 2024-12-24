56fe140f30e4afc2352a9fb9feb56b8f6a4c46fb
terezasereza <terka.valkova02@seznam.cz>
2024-12-25 00:30:54 +0100

    dwafgh

--- js/main.js
+++ js/main.js
@@ -3,11 +3,26 @@ import('./audio.js').then(module => {
     window.audioManager = new module.AudioManager();
 });
 
-// Načíst deník až po interakci
-document.querySelector('.diary-icon').addEventListener('click', () => {
+// Načteme deník ihned při načtení stránky
+document.addEventListener('DOMContentLoaded', () => {
     import('./diary.js').then(module => {
         if (!window.diary) {
             window.diary = new module.Diary();
+            // Otevřeme deník po krátké prodlevě, aby se vše stihlo načíst
+            setTimeout(() => {
+                window.diary.openDiary();
+            }, 500);
         }
     });
+});
+
+// Ponecháme i listener pro ikonu
+document.querySelector('.diary-icon').addEventListener('click', () => {
+    if (window.diary) {
+        window.diary.toggleDiary();
+    } else {
+        import('./diary.js').then(module => {
+            window.diary = new module.Diary();
+        });
+    }
 }); 
